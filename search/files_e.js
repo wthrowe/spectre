6ea036d7ff5657fdae2b29e2a98fea93116bfd74
Automatic Deployment (Travis CI) <none@none.none>
2017-05-31 01:48:36 +0000

    Documentation Update: fa72d3c70d5e3e732b4abe4671a111f7a93746e8

--- search/files_e.js
+++ search/files_e.js
@@ -2,7 +2,6 @@ var searchData=
 [
   ['random',['random',['http://en.cppreference.com/w/cpp/header/random.html',1,'']]],
   ['ratio',['ratio',['http://en.cppreference.com/w/cpp/header/ratio.html',1,'']]],
-  ['readme_2emd',['README.md',['../README_8md.html',1,'']]],
   ['regex',['regex',['http://en.cppreference.com/w/cpp/header/regex.html',1,'']]],
   ['runtests_2ecpp',['RunTests.cpp',['../RunTests_8cpp.html',1,'']]],
   ['runtests_2ehpp',['RunTests.hpp',['../RunTests_8hpp.html',1,'']]]
--- search/pages_6.js
+++ search/pages_6.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['what_20is_20spectre_3f',['What is SpECTRE?',['../md_README.html',1,'']]],
   ['writing_20good_20documentation',['Writing Good Documentation',['../writing_good_dox.html',1,'']]]
 ];
