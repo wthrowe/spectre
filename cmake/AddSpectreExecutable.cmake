7171e6850d8e580ae5d9b9f475a12f6f3c3fcb92
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2020-12-18 21:17:39 +0100

    Link Charm++ as a CMake target
    Remove the Charm++ compiler wrapper `charmc` from our
    build system. Instead, retrieve include directories, libraries and
    compiler flags from it and prepare a CMake target that our
    libraries and executables can link in like any other.
    
    This helps making builds with shared libraries more robust,
    and was necessary for me to get shared lib builds working
    on macOS.
    
    Also removes almost all CharmCompatibility code from Pybindings. They are
    not needed anymore because the Charm++ libs are now linked correctly.

--- cmake/AddSpectreExecutable.cmake
+++ cmake/AddSpectreExecutable.cmake
@@ -36,6 +36,7 @@ function(add_spectre_executable TARGET_NAME)
   target_link_libraries(
     ${TARGET_NAME}
     PRIVATE
+    CharmModuleInit
     SpectreFlags
     )
 endfunction()
@@ -103,6 +104,7 @@ function(
     # Link against Boost::program_options for now until we have proper
     # dependency handling for header-only libs
     Boost::program_options
+    Charmxx::main
     ${LINK_LIBS}
     )
 endfunction(add_spectre_parallel_executable)
