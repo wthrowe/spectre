dd18be2869dd645fda93e3376b99fca78f95bbf7
William Throwe <wtt6@cornell.edu>
2023-01-06 17:31:17 -0500

    Use history instead of separate rollback values

--- src/Evolution/DgSubcell/Actions/Initialize.hpp
+++ src/Evolution/DgSubcell/Actions/Initialize.hpp
@@ -29,7 +29,6 @@
 #include "NumericalAlgorithms/Spectral/Mesh.hpp"
 #include "Parallel/AlgorithmExecution.hpp"
 #include "Parallel/GlobalCache.hpp"
-#include "Time/Tags.hpp"
 #include "Utilities/ContainerHelpers.hpp"
 #include "Utilities/TMPL.hpp"
 #include "Utilities/TaggedTuple.hpp"
@@ -95,7 +94,6 @@ struct Initialize {
 
   using simple_tags =
       tmpl::list<Tags::ActiveGrid, Tags::DidRollback,
-                 ::Tags::RollbackValue<typename System::variables_tag>,
                  Tags::TciGridHistory, Tags::NeighborDataForReconstruction<Dim>,
                  Tags::TciDecision, Tags::NeighborTciDecisions<Dim>,
                  Tags::DataForRdmpTci,
