26fbcfe335e33614f82f4fb6c97bba8e0ba04464
William Throwe <wtt6@cornell.edu>
2023-02-23 00:47:40 -0500

    Add History::insert_in_place

--- tests/Unit/Time/Test_History.cpp
+++ tests/Unit/Time/Test_History.cpp
@@ -162,8 +162,11 @@ void test_history() {
   CHECK(not const_history[2].value.has_value());
   CHECK(const_history[0].value.has_value());
   CHECK(const_history[3].value.has_value());
-  history.insert(TimeStepId(true, 0, slab.start() + slab.duration() / 2),
-                 make_value(2.0), make_deriv(20.0));
+  history.insert_in_place(
+      TimeStepId(true, 0, slab.start() + slab.duration() / 2),
+      // Avoid overwriting the cached allocation for testing.
+      [&](const auto v) { *v = as_const(make_value(2.0)); },
+      [&](const auto d) { *d = as_const(make_deriv(20.0)); });
   // [(-1/2, -3, -300), (-1/4, X, -20), (0, X, -100), (1/4, 1, 10),
   //  (1/2, 2, 20)] []
   if constexpr (tt::is_a_v<Variables, Vars>) {
@@ -403,9 +406,14 @@ void test_history() {
   history.insert(TimeStepId(true, 1, step_time2, 1, slab.end().value()),
                  History::no_value, make_deriv(70.0));
   // [(1/4, X, 10), (1/2, 2, 20), (3/4, 6, 60)] [3/4: (1, X, 70)]
-  history.insert(TimeStepId(true, 1, step_time2, 2, slab.end().value()),
-                 make_value(8.0), make_deriv(80.0));
-  // [(1/4, X, 10), (1/2, 2, 20), (3/4, 6, 60)] [3/4: (1, X, 70), (1, 8, 80)]
+  history.insert_in_place(
+      TimeStepId(true, 1, step_time2, 2, slab.end().value()), History::no_value,
+      [&](const auto d) { *d = as_const(make_deriv(80.0)); });
+  // [(1/4, X, 10), (1/2, 2, 20), (3/4, 6, 60)] [3/4: (1, X, 70), (1, X, 80)]
+  history.insert(TimeStepId(true, 1, step_time2, 3, slab.end().value()),
+                 make_value(9.0), make_deriv(90.0));
+  // [(1/4, X, 10), (1/2, 2, 20), (3/4, 6, 60)]
+  //     [3/4: (1, X, 70), (1, X, 80), (1, 9, 90)]
 
   if constexpr (tt::is_a_v<Variables, Vars>) {
     CHECK(const_history.substeps().back().value->data() == cached_value);
@@ -413,7 +421,7 @@ void test_history() {
   }
 
   history.undo_latest();
-  // [(1/4, X, 10), (1/2, 2, 20), (3/4, 6, 60)] [3/4: (1, X, 70)]
+  // [(1/4, X, 10), (1/2, 2, 20), (3/4, 6, 60)] [3/4: (1, X, 70), (1, X, 80)]
   history.shrink_to_fit();
 
   // Allocate some memory to reduce the likelihood that a new entry
@@ -421,9 +429,10 @@ void test_history() {
   [[maybe_unused]] const auto created_value = make_value(0.0);
   [[maybe_unused]] const auto created_deriv = make_deriv(0.0);
 
-  history.insert(TimeStepId(true, 1, step_time2, 2, slab.end().value()),
-                 make_value(8.0), make_deriv(80.0));
-  // [(1/4, X, 10), (1/2, 2, 20), (3/4, 6, 60)] [3/4: (1, X, 70), (1, 8, 80)]
+  history.insert(TimeStepId(true, 1, step_time2, 3, slab.end().value()),
+                 make_value(9.0), make_deriv(90.0));
+  // [(1/4, X, 10), (1/2, 2, 20), (3/4, 6, 60)]
+  //     [3/4: (1, X, 70), (1, X, 80), (1, 8, 80)]
 
   if constexpr (tt::is_a_v<Variables, Vars>) {
     CHECK(const_history.substeps().back().value->data() != cached_value);
@@ -449,7 +458,7 @@ void test_history() {
 
   history.map_entries([](const auto entry) { *entry *= 10.0; });
   // [(1/4, X, 100), (1/2, 20, 200), (3/4, 60, 600)]
-  // [3/4: (1, X, 700), (1, 8, 800)]
+  // [3/4: (1, X, 700), (1, X, 800), (1, 9, 900)]
   CHECK(history.size() == 3);
   for (size_t i = 0; i < history.size(); ++i) {
     CHECK(history[i].time_step_id == copy[i].time_step_id);
@@ -459,7 +468,7 @@ void test_history() {
     }
     CHECK(history[i].derivative == Derivs(10.0 * copy[i].derivative));
   }
-  CHECK(history.substeps().size() == 2);
+  CHECK(history.substeps().size() == 3);
   for (size_t i = 0; i < history.substeps().size(); ++i) {
     CHECK(history.substeps()[i].time_step_id ==
           copy.substeps()[i].time_step_id);
